6a8b9133bd08186eb04c3901498557d4b404e103
Cerulity64X <cerulity32k@gmail.com>
2022-03-07 21:01:17 -0500

    Initial website commit

---
+++ reqs.js
@@ -0,0 +1,14 @@
+let req = new XMLHttpRequest();
+let resp = "No request yet!"
+
+const url='cerulity32k.github.io';
+req.open("GET", url);
+req.send();
+
+req.onreadystatechange = (e) => {
+    resp = req.responseText;
+}
+
+function up() {
+    document.getElementById("upBtn").innerHTML = resp;
+}
