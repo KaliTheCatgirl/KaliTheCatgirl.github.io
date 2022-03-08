12a73073b58734a83dd0b8b1721d13b9d9721eba
Cerulity64X <cerulity32k@gmail.com>
2022-03-08 09:21:52 -0500

    Change API
    Change the request API from XMLHttpRequest to Fetch.
    Also added a WIP Instruction enum for scripting, and changed indentation from spaces to tabs.

--- reqs.tsx
+++ reqs.tsx
@@ -1,12 +1,41 @@
+enum Instructions
+{
+	Nop,
+	Up,
+	Down,
+	Forward,
+	Left,
+	Right,
+	Mine,
+	MineUp,
+	MineDown,
+	Place,
+	PlaceUp,
+	PlaceDown,
+	Select
+}
 document.getElementById("scriptWorks").innerHTML = "Script loaded and working!";
 let url = "";
 
-function up() {
-    let req = new XMLHttpRequest();
-    req.open("POST", url + "?code=up");
-    req.send();
+async function get(url: string = '', data: {} = {}) {
+	const response = await fetch(url, {
+		cache: 'no-cache',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		referrerPolicy: 'no-referrer',
+		body: JSON.stringify(data)
+	});
+	return response.json();
+  }
+
+function up() {  
+	get(url, { code: Instructions.Up })
+		.then(data => {
+			console.log(data);
+		});
 }
 
 function updateurl() {
-    url = document.getElementById("urlBox").innerHTML;
+	url = document.getElementById("urlBox").innerHTML;
 }
